950d69557059e79046c176a375abe4755296265d
Elana Kopelevich <ekopelevich@gmail.com>
2018-11-17 20:44:44 -0700

    wip - wherever I was at with notes..

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
+  layout 'dashboard'
 
   # GET /messages
   # GET /messages.json
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :messages
   root 'welcome#index'
   resources :clubs
   resources :events
+  resources :messages
+  resources :notes
   resources :resources
   resources :topics
   devise_for :users
